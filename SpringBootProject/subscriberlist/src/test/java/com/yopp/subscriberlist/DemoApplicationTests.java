ac3cfbfd5f4e4eb2526e4e69e8df69726bfd3770
Rumble Roar <lnyopp15@gmail.com>
2020-06-22 08:37:37 -0400

    initial push setup  springboot set up

---
+++ SpringBootProject/subscriberlist/src/test/java/com/yopp/subscriberlist/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yopp.subscriberlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
