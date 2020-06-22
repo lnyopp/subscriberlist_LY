83f9650455d87fe592f66d920a58da5ba817c85f
Rumble Roar <lnyopp15@gmail.com>
2020-06-22 11:01:27 -0400

    start of thymeleaf create a subscriber controller

--- SpringBootProject/subscriberlist/src/main/java/com/yopp/subscriberlist/subscriber/SubscriberController.java
+++ SpringBootProject/subscriberlist/src/main/java/com/yopp/subscriberlist/subscriber/SubscriberController.java
@@ -1,13 +1,37 @@
 package com.yopp.subscriberlist.subscriber;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
 
 @Controller
 public class SubscriberController {
+
+    @Autowired
+    private SubscriberRepository subscriberRespository;
+
     @GetMapping
     public String index(Subscriber subscriber){
         return "subscriber/index";
     }
+
+    private Subscriber subscriber;
+
+    public String addNewSubscriber(Subscriber subscriber, Model model){
+        subscriberRespository.save(new Subscriber(subscriber.getFirstName(),subscriber.getLastName(),subscriber.getUserName(),subscriber.getSignedUp()));
+        model.addAttribute("firstName", subscriber.getFirstName());
+        model.addAttribute("lastName", subscriber.getLastName());
+        model.addAttribute("userName", subscriber.getUserName());
+
+        return "subscriber/result";
+
+        
+    }
+
     
 }
